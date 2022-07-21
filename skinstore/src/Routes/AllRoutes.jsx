5c4f0915d27455d7ea481e879d6a553b5dce8617
PoojaD12345 <dhuripooja01@gmail.com>
2022-07-21 00:35:09 -0700

    Navbar,Footer done

---
+++ skinstore/src/Routes/AllRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../components/Footer'
+// import { Route, Routes } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+
+const AllRoutes = () => {
+  return (
+    <div>
+     <Navbar/>
+    {/* <Routes>
+        <Route/>
+
+    </Routes> */}
+    <Footer/>
+    </div>
+  )
+}
+
+export default AllRoutes
