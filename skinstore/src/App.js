9e909809ca6aa5d33c49f13198ba0642ebcc322f
PoojaD12345 <dhuripooja01@gmail.com>
2022-07-22 23:40:55 -0700

    login and registration functionality done

--- skinstore/src/App.js
+++ skinstore/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import AllRoutes from './Routes/AllRoutes';
-import Register from './Routes/Register';
 
 function App() {
   return (
     <div className="App">
      <AllRoutes/>
-     {/* <Register/> */}
     </div>
   );
 }
---
+++ skinstore/src/Context/AuthContext.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react';
+
+export const AuthContext=React.createContext();
+
+function AuthContextProvider({children}){
+        const [isAuth,setIsAuth]=useState(false)
+           const [getlog,setGetlog]=useState({
+              email:"",password:""
+           })
+           const [getdata,setGetData]=useState({
+            email1:"",password1:"",fname1:""
+           })
+
+           const bringForm=(email1,password1)=>{
+            setGetData({
+                ...getdata,email1,password1
+            })
+            // console.log(email1,password1)
+           }
+           const bringLoginform=(email,password)=>{
+            setGetlog({
+                ...getlog,email,password
+            })
+            console.log(email,password)
+           }
+
+           const login=(email,password,email1,password1)=>{
+                  if(email===email1 && password===password1){
+                    setIsAuth(true)
+                  }
+           }
+           const logout=()=>{
+            setIsAuth(false)
+           }
+return (
+    <AuthContext.Provider value={{bringForm,bringLoginform,login,logout,isAuth}}>{children}</AuthContext.Provider>
+)
+}
+export default AuthContextProvider
